a565f1479bca982cbc9a8edf58795ed48ad8b458
jwpark <jwpark@kimzipsa.co.kr>
2021-10-21 22:59:52 +0900

    정보 추가

--- src/components/Lunch.vue
+++ src/components/Lunch.vue
@@ -31,9 +31,11 @@
                     당신에게 내려진 메뉴는
                 </div>
                 <div class="result-coin rst-up"></div>
-                <div id="test" class="result-coin-changed">             
+                <div class="result-coin-changed">             
                     <p class="result-coins rst-up">메뉴명: <span class="" id="menu"></span></p>       
-                    <p class="result-coins rst-up">칼로리: <span class="" id="kcal"></span></p>
+                    <p class="result-coins rst-up">칼로리: <span class="" id="Cal"></span></p>
+                    <p class="coin-down">성인 하루 권장칼로리</p>        
+                    <span class="txt-daily">남자: 2,500Cal</span> - <span class="txt-daily">여자: 2,000Cal</span>            
                     
                 </div>                
                 <div class="" style="margin-top: 3%;">
@@ -41,16 +43,10 @@
                     <label for="cbbob" class="cb">밥</label>
                     <input type="checkbox" id="cbmyun" name="chk" value="면"/>
                     <label for="cbmyun" class="cb">면</label>       
-                    <input type="checkbox" id="cbt" name="chk" value="튀긴거"/>
-                    <label for="cbt" class="cb">튀긴거</label>     
-                    <input type="checkbox" id="cbg" name="chk" value="구운거"/>
-                    <label for="cbg" class="cb">구운거</label>   
-                    <input type="checkbox" id="cbn" name="chk" value="날거"/>
-                    <label for="cbn" class="cb">날거</label>      
                     <input type="checkbox" id="cbj" name="chk" value="찌개"/>
-                    <label for="cbj" class="cb">찌개</label>     
-                    <input type="checkbox" id="cbb" name="chk" value="빵"/>
-                    <label for="cbb" class="cb">빵</label>                                                                                              
+                    <label for="cbj" class="cb">국물</label>     
+                    <input type="checkbox" id="cbb" name="chk" value="간편"/>
+                    <label for="cbb" class="cb">간편</label>                                                                                              
                 </div>                
                 <div class="content-text">
                     메뉴를 선택 후 <br>귀여운 돼지를 클릭해 <br>점심추천을 받아보세요
@@ -80,61 +76,98 @@ export default {
   name: 'Lunch',
   data(){
       return{
-        datas:{
-            "밥": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }],  
-            "면": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }],
-            "찌개": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }],
-            "튀긴거": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }],    
-            "구운거": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }],    
-            "날거": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }],  
-            "빵": [{
-                "name": "김치찌깨",
-                "kcal": "420"                
-            }]                                                                        
-        },
         foods: [
             {name: '밥', data: [
-                {"name":"볶음밥", 'kcal':"222"},
-                {"name":"비빔밥", 'kcal':"222"},
-                {"name":"오므라이스", 'kcal':"222"},
-                {"name":"제육덮밥", 'kcal':"222"},
-                {"name":"오징어덮밥", 'kcal':"222"},
+                {"name":"새우볶음밥", 'Cal':"241"},
+                {"name":"비빔밥", 'Cal':"586"},
+                {"name":"오므라이스", 'Cal':"380"},
+                {"name":"제육덮밥", 'Cal':"779"},
+                {"name":"오징어덮밥", 'Cal':"667"},                
+                {"name":"육회비빔밥", 'Cal':"531"},
+                {"name":"돈까스", 'Cal':"568"},
+                {"name":"일식돈까스", 'Cal':"565"},
+                {"name":"가츠동", 'Cal':"650"},
+                {"name":"규동", 'Cal':"614"},
+                {"name":"초밥", 'Cal':"개당 37"},
+                {"name":"마파두부밥", 'Cal':"360"},
+                {"name":"보쌈정식", 'Cal':"844"},
+                {"name":"백반", 'Cal':"830"},
+                {"name":"생선구이", 'Cal':"406"},
+                {"name":"뚝배기불고기", 'Cal':"317"},
+                {"name":"한식부페", 'Cal':"먹는만큼"},
+                {"name":"김치볶음밥", 'Cal':"446"},
+                {"name":"쌈밥", 'Cal':"좀만먹어"},
+                {"name":"돈부리", 'Cal':"655"},
+                {"name":"연어덮밥", 'Cal':"574"},
+                {"name":"장어덮밥", 'Cal':"626"},
+                {"name":"카레", 'Cal':"544"},
+                {"name":"잡채밥", 'Cal':"673"},
             ]},
             {name: '면', data: [
-                {"name":"", 'kcal':""}
+                {"name":"짜장면", 'Cal':"785"},
+                {"name":"짬뽕", 'Cal':"764"},
+                {"name":"쌀국수", 'Cal':"320"},
+                {"name":"우동", 'Cal':"361"},
+                {"name":"메밀국수", 'Cal':"113"},
+                {"name":"크림파스타", 'Cal':"275"},
+                {"name":"봉골레", 'Cal':"479"},
+                {"name":"토마토파스타", 'Cal':"116"},
+                {"name":"칼국수", 'Cal':"420"},
+                {"name":"물냉면", 'Cal':"383"},
+                {"name":"비빔냉면", 'Cal':"463"},
+                {"name":"로제파스타", 'Cal':"225"},
+                {"name":"잔치국수", 'Cal':"447"},
+                {"name":"콩국수", 'Cal':"513"},
+                {"name":"비빔국수", 'Cal':"425"},
+                {"name":"막국수", 'Cal':"452"},
+                {"name":"일본라멘", 'Cal':"395"},
+                {"name":"라면", 'Cal':"385"},
             ]},
-            {name: '찌개', data: [
-                {"name":"", 'kcal':""}
+            {name: '국물', data: [
+                {"name":"짬뽕밥", 'Cal':"482"},
+                {"name":"김치찌개", 'Cal':"128"},
+                {"name":"된장찌개", 'Cal':"171"},
+                {"name":"부대찌개", 'Cal':"626"},
+                {"name":"육개장", 'Cal':"165"},
+                {"name":"뼈해장국", 'Cal':"309"},
+                {"name":"순대국밥", 'Cal':"360"},
+                {"name":"샤브샤브", 'Cal':"384"},
+                {"name":"마라탕", 'Cal':"175"},
+                {"name":"선지해장국", 'Cal':"430"},
+                {"name":"설렁탕", 'Cal':"424"},
+                {"name":"갈비탕", 'Cal':"466"},
+                {"name":"순두부찌개", 'Cal':"259"},
+                {"name":"청국장", 'Cal':"117"},
+                {"name":"콩나물국밥", 'Cal':"445"},
+                {"name":"삼계탕", 'Cal':"454"},
+                {"name":"오징어찌개", 'Cal':"163"},
+                {"name":"비지찌개", 'Cal':"121"},                
+                {"name":"추어탕", 'Cal':"194"},
+                {"name":"만두국", 'Cal':"418"},
+                {"name":"동태탕", 'Cal':"158"},
             ]},
-            {name: '튀긴거', data: [
-                {"name":"", 'kcal':""}
-            ]},
-            {name: '구운거', data: [
-                {"name":"", 'kcal':""}
-            ]},
-            {name: '날거', data: [
-                {"name":"", 'kcal':""}
-            ]},
-            {name: '빵', data: [
-                {"name":"", 'kcal':""}
+            {name: '간편', data: [
+                {"name":"라면", 'Cal':"385"},
+                {"name":"김밥", 'Cal':"485"},
+                {"name":"햄버거", 'Cal':"426"},
+                {"name":"편의점도시락", 'Cal':"720"},
+                {"name":"샐러드", 'Cal':"16"},
+                {"name":"샌드위치", 'Cal':"252"},
+                {"name":"떡튀순", 'Cal':"280"},
+                {"name":"핫도그", 'Cal':"242"},
+                {"name":"시리얼", 'Cal':"124"},
+                {"name":"토스트", 'Cal':"420"},
+                {"name":"삼각김밥", 'Cal':"174"},
+                {"name":"핫바", 'Cal':"184"},
+                {"name":"오뎅", 'Cal':"70"},
+                {"name":"다이어트", 'Cal':"죽을만큼 뛰어라"},
+                {"name":"물", 'Cal':"0"},
+                {"name":"부리또", 'Cal':"253"},
+                {"name":"피자", 'Cal':"조각당 255"},
+                {"name":"치킨", 'Cal':"조각당 249"},
+                {"name":"죽", 'Cal':"214"},
+                {"name":"고구마", 'Cal':"193"},
+                {"name":"계란", 'Cal':"77"},
             ]},                                                            
         ]          
       }      
@@ -144,14 +177,6 @@ export default {
   },
   methods:{
     fnInit(){
-        // const open = () => {
-        //     document.querySelector(".modal").classList.remove("hidden");
-        //     console.log('open');
-        // }
-        // const close = () => {
-        //     document.querySelector(".modal").classList.add("hidden"); 
-        // } 
-
 
         let replay = document.querySelector(".replay")
         let contentExplanation = document.querySelector(".content-explanation")
@@ -171,11 +196,7 @@ export default {
         for (let i = 0; i < loadingText.length; i++) {
             let item = loadingText.item(i);
             item.style.display = 'none'
-        }        
-
-        // document.querySelector(".open-btn").addEventListener("click",open);
-        // document.querySelector(".modal").addEventListener("click",close);
-        // document.querySelector(".bg").addEventListener("click",close);      
+        }          
     },
     fnEvent(){
         let contentText = document.querySelector(".content-text")
@@ -251,49 +272,13 @@ export default {
         this.fnWrite(menuData)   
     },
     fnWrite(menuData){
-//     setTimeout(function() {
-//         $('.loading-img').hide();
-//         $('.loading-text').hide();
-//         $('.location').hide();
-//         $('.replay').show();
-//         $('.result-coin').show();
-//         $('.content-explanation').show();
-//         $('.result-img').show();
-//         $('.result-coin-changed').show()
-//     },2000);    
-
-
-
-
-//     const coinShow = document.querySelector(".result-coin");
-//     const price = document.querySelector(".result-coin-tradeprice")
-//     const signed = document.querySelector(".result-coin-signedchange")
-//     const high = document.querySelector(".result-coin-highprice")
-//     const low = document.querySelector(".result-coin-lowprice")
-
-//     let css = document.getElementsByClassName("result-coin-changed")
-//     let cls = 'coin-up'              
-
-//     $(css).addClass("up")
-//     if(parseFloat((data[0].signed_change_rate * 100)) < 0){
-//         cls = 'coin-down'
-//         $(css).removeClass("up").addClass("down")
-//     }
-
-//     coinShow.innerText = upbits.korean_name;
-//     price.innerText = data[0].trade_price.toLocaleString()
-//     signed.innerText = `${(data[0].signed_change_rate * 100).toFixed(2).toLocaleString()}%`
-//     high.innerText = data[0].high_price.toLocaleString()
-//     low.innerText = data[0].low_price.toLocaleString()      
-
-
         const target = Object.assign({}, menuData.data);
-        console.log(target[this.fnRand(Object.keys(target))].name)   
-        console.log(target[this.fnRand(Object.keys(target))].kcal)   
+        const index = this.fnRand(Object.keys(target))        
+        const menu = target[index].name
+        const kcal = target[index].Cal
 
-
-        document.getElementById('menu').innerText = target[this.fnRand(Object.keys(target))].name
-        document.getElementById('kcal').innerText = `${target[this.fnRand(Object.keys(target))].kcal}kcal`
+        document.getElementById('menu').innerText = menu
+        document.getElementById('Cal').innerText = `${kcal} 칼로리`
 
     }        
   }
@@ -311,6 +296,7 @@ export default {
     vertical-align: middle;    
     /* padding: 3% 1% 1% 1%; */
     margin-left: 2px;        
+    line-height: 50px;
 }
 .cb2 {
 	width: 50px;
